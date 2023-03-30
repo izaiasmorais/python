62fa56344611d19e717c9e7f46382174e059d0b5
Izaias <izaiaslima356@gmail.com>
2023-03-30 10:01:15 -0300

    chore: study data types and operators

---
+++ 1 - logic/arithmetics-operators.py
@@ -0,0 +1,19 @@
+
+adition = 10 + 10
+subtraction = 10 - 5
+multiplication = 10 * 10
+division = 10 / 2.0
+integer_division = 10 // 2.0
+exponentiation = 10 ** 2
+modules = 25 % 2
+
+# rest of division is 0
+x = 4
+if 25 % x == 0:
+  print("25 is divisible by", x)
+else:
+	print("25 is not divisible by", x)
+
+# the operators can do another thing too
+print("A" + "B") # concat
+print("B" * 10) # repeat
---
+++ 1 - logic/data-types-exercise.py
@@ -0,0 +1,13 @@
+name = 'Izaías'
+surname = 'Lima'
+age = 19
+birth_year = 2022 - age # 2003
+of_age = age >= 18
+height = 1.83
+
+print("Name: ",  name)
+print("Surname: ",  surname)
+print("Age: ",  age)
+print("Birth year: ",  birth_year)
+print("Are you of age?: ",  of_age)
+print("Hight in meters: ",  height)
