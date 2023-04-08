f130da8a6d1cb271534faed0458e6c79a6448909
Izaias <izaiaslima356@gmail.com>
2023-04-07 21:13:13 -0300

    study input function and conditionals

---
+++ 1 - logic/conditionals.py
@@ -0,0 +1,13 @@
+
+
+option = input("Type W or L: ")
+
+if (option == "W"):
+	print("We win")
+elif(option == "L"):
+	print("We losed")
+else:
+	print("You didn't type W or L")
+
+
+print("Code has finished!")
---
+++ 1 - logic/input.py
@@ -0,0 +1,8 @@
+
+name = input("Type your name: ")
+
+print(f"Your name is {name}")
+
+age = input("Type you age: ")
+
+print(f"You {age} years old")
