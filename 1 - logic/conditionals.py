91700cd41121aa85c965eadd0948062c43acb1e6
Izaias <izaiaslima356@gmail.com>
2023-05-15 22:23:32 -0300

    chore: study and, or, in and string interpolation

--- 1 - logic/conditionals.py
+++ 1 - logic/conditionals.py
@@ -1,13 +1,10 @@
-
-
 option = input("Type W or L: ")
 
 if (option == "W"):
-	print("We win")
-elif(option == "L"):
-	print("We losed")
+    print("We win")
+elif (option == "L"):
+    print("We losed")
 else:
-	print("You didn't type W or L")
-
+    print("You didn't type W or L")
 
 print("Code has finished!")
---
+++ 1 - logic/interpolation.py
@@ -0,0 +1,6 @@
+product = 'car'
+price = 100000.00
+
+line = "The %s costs %.1f" % (product, price)
+
+print(line)
