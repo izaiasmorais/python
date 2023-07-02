8c8b6f268d8a96f0f6fee609bda2e0a6fafb9ef7
Izaias <izaiaslima356@gmail.com>
2023-07-02 09:02:36 -0300

    chore: study cut strings

--- 1 - logic/3-arithmetics-operators.py
+++ 1 - logic/3-arithmetics-operators.py
@@ -10,10 +10,10 @@ modules = 25 % 2
 # rest of division is 0
 x = 4
 if 25 % x == 0:
-  print("25 is divisible by", x)
+    print("25 is divisible by", x)
 else:
-	print("25 is not divisible by", x)
+    print("25 is not divisible by", x)
 
 # the operators can do another thing too
-print("A" + "B") # concat
-print("B" * 10) # repeat
+print("A" + "B")  # concat
+print("B" * 10)  # repeat
---
+++ 1 - logic/7-cut-strings.py
@@ -0,0 +1,7 @@
+
+string = "Hello world"
+
+print(string[6:11])  # world
+
+# To invert a string we use
+print(string[::-1])
